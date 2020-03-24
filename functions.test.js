f073cf23f233e84385ecad00a980fa477089913a
gergokutu <gergo.kutu@gmail.com>
2020-03-24 17:47:43 +0100

    Implement a NOT toBe test on add()

--- functions.test.js
+++ functions.test.js
@@ -6,3 +6,9 @@ const { add } = functions
 test(' Add 1 + 2 equals to 3', () => {
   expect(add(1, 2)).toBe(3)
 })
+
+test('2nd test does nothing...', () => {})
+
+test('Add 1 + 2 NOT eguals to 4', () => {
+  expect(add(1, 2)).not.toBe(4)
+})
